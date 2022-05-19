0e0b187e7a762d31982bce81a525bf08431af6c1
bmykhaylivvv <bmykhaylivvv@gmail.com>
2022-05-19 23:10:27 +0300

    Add POST request for uploading files to S3

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -14,6 +16,7 @@ import (
 var AccessKeyID string
 var SecretAccessKey string
 var MyRegion string
+var filepath string
 
 func LoadEnv() {
 	err := godotenv.Load(".env")
@@ -33,6 +36,7 @@ func ConnectAws() *session.Session {
 	AccessKeyID = GetEnvWithKey("AWS_ACCESS_KEY_ID")
 	SecretAccessKey = GetEnvWithKey("AWS_SECRET_ACCESS_KEY")
 	MyRegion = GetEnvWithKey("AWS_REGION")
+
 	sess, err := session.NewSession(
 		&aws.Config{
 			Region: aws.String(MyRegion),
@@ -42,14 +46,42 @@ func ConnectAws() *session.Session {
 				"", // a token will be created when the session it's used.
 			),
 		})
+
 	if err != nil {
 		panic(err)
 	}
+
 	return sess
 }
 
 func UploadImage(c *gin.Context) {
-	// codes that will parse file and upload to amazon s3 bucket
+	sess := c.MustGet("sess").(*session.Session)
+	uploader := s3manager.NewUploader(sess)
+	MyBucket := GetEnvWithKey("BUCKET_NAME")
+	file, header, err := c.Request.FormFile("file")
+	filename := header.Filename
+
+	//upload to the s3 bucket
+	up, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(MyBucket),
+		ACL:    aws.String("public-read"),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":    "Failed to upload file",
+			"uploader": up,
+		})
+
+		return
+	}
+
+	filepath = "https://" + MyBucket + "." + "s3.amazonaws.com/" + filename
+	c.JSON(http.StatusOK, gin.H{
+		"filepath": filepath,
+	})
 }
 
 func main() {
@@ -65,5 +97,5 @@ func main() {
 
 	router.POST("/upload", UploadImage)
 
-	_ = router.Run(":4000")
+	_ = router.Run(":5000")
 }
